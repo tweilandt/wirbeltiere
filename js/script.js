9465b95c33b4f3d39f3e968ac996300524f855ac
tweilandt <traiuga@hotmail.com>
2024-05-26 12:06:54 +0200

    style_voegel

--- js/script.js
+++
@@ -1,9 +0,0 @@
-let itens = document.getElementById('itens')
-
-function clickMenu(){
-    if (itens.style.display == 'block'){
-        itens.style.display = 'none'
-    } else {
-        itens.style.display = 'block'
-    }
-}
