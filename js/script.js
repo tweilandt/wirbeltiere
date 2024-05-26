3339f195b788478c419278ceac7c4250f89c40bf
tweilandt <traiuga@hotmail.com>
2024-05-26 10:16:48 +0200

    add js ext

---
+++ js/script.js
@@ -0,0 +1,9 @@
+let itens = document.getElementById('itens')
+
+function clickMenu(){
+    if (itens.style.display == 'block'){
+        itens.style.display = 'none'
+    } else {
+        itens.style.display = 'block'
+    }
+}
